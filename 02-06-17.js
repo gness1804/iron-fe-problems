5d1e340c66fd39acc3721eb2ae5f68a03c6373ef
gness1804 <graham.nessler@gmail.com>
2017-02-05 15:36:23 -0700

    Add Feb 6 problems and correct date typo for Jan 26 problems

---
+++ 02-06-17.js
@@ -0,0 +1,9 @@
+//write a function that finds the character(s) in the middle of a string. If the string's length is even, return the middle two chars; if the string's length is odd, return the middle char.
+
+//i.e.: 'duck' should return 'uc'; 'shoot' should return 'o'
+
+//the argument passed in should be a string comprised of a single word, link the two examples above 
+
+const findMiddle = (array) => {
+
+}
